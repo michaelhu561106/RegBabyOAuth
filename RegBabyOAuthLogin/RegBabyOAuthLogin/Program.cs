07cd0e5739e7e9aede783ca5fa8afa67a13590ca
LAPTOP-BJBNG77F\user <michaelhu561106@gmail.com>
2025-02-07 12:07:31 +0800

    Create Project

---
+++ RegBabyOAuthLogin/RegBabyOAuthLogin/Program.cs
@@ -0,0 +1,31 @@
+using RegBabyOAuthLogin.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(RegBabyOAuthLogin.Client._Imports).Assembly);
+
+app.Run();
